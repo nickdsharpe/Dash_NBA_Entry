4a8ed71064121996bebc4e7caedfe1dfd5c01a32
nicksharpe <nickdsharpe@comcast.net>
2023-08-15 20:10:54 -0600

    Home

--- construction/json_optimization.py
+++ construction/json_optimization.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import json
 
-with open(f'../game_data/team_one/Offense/Murray.json', 'r', encoding='utf-8') as f:
+with open(f'../game_data/team_one/Offense/Jokic.json', 'r', encoding='utf-8') as f:
         file = json.load(f)
         
 df = pd.DataFrame(file).transpose()
